fe7f54eadb10272dabf8c04977b1ffba5cb05c4b
eyob worku <eyobworku62@gmail.com>
2025-06-30 18:24:20 +0300

    refactor: update ESLint configuration to use compat.config for better rule management

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@next/next/no-sync-scripts": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
